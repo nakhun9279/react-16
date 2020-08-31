607ef3ef04c4f1f0a60c3e34b15f4093e8bddaff
nakhun9279 <fjfj9279@naver.com>
2020-08-31 14:18:17 +0900

    16.2.5 초깃값 설정

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ const DECREASE = 'DECREASE';
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = difference => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
+const initialState = {
+    toggle: false,
+    counter: 0
+  };
